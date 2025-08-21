d092faccd45218ea843786acf22d796e588e4e6e
chilejay7 <codyburk7@yahoo.com>
2025-08-21 06:07:05 -0600

    headers added

--- monitor.js
+++ monitor.js
@@ -4,7 +4,14 @@ const url = "https://www.thebriarfellowship.com/product-page/pre-order-raven-cla
 
 const monitor = async () => {
     
-    const response = await got(url);
+    // Headers are needed to lend the request more validity when received by the server.  At times when there is increased traffic for a really popular item, headers are necessary to allow the request through.  It will more closely resemble regular browser requsets from a user.
+    
+    const myHeaders = {
+
+    }
+    const response = await got(url, {
+        headers: myHeaders
+    });
     console.log(response.statusCode);
 };
 
