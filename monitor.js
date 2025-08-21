4c245244748ebfe08680392abe1c61a73201a84e
chilejay7 <codyburk7@yahoo.com>
2025-08-20 23:34:36 -0600

    async monitor function created

--- monitor.js
+++ monitor.js
@@ -1,2 +1,9 @@
 const got = require('got');
 
+const url = "https://www.thebriarfellowship.com/product-page/pre-order-raven-claw-orb-tamper-in-two-finishes";
+
+const monitor = async () => {
+    const response =- await got(url);
+
+    console.log(response.statusCode);
+}
